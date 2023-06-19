a1e0e3baefb30969536225968bca29b77d5ebe69
VIKAS <95766966+vksmadheshiya@users.noreply.github.com>
2023-06-19 13:50:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+import pandas as pd
 from repo_complexity import find_most_complex_repository
 
 # streamlit framework
@@ -8,12 +9,14 @@ github_url=st.text_input("Please Enter The GitHub User's URL For Analysis")
 
 
 # Example github_url = 'https://github.com/vksmadheshiya'
-most_complex_repository = find_most_complex_repository(github_url)
+most_complex_repository, complexity_report = find_most_complex_repository(github_url)
 
 
 if github_url:
-    if most_complex_repository:
+    if most_complex_repository :
         st.write(f" # The most technically complex repository from user '{github_url}' is '{most_complex_repository}'.")
+        st.write('Below is a Complexity Report:', complexity_report)
+
     else:
         st.write("Some Error Occured")
     
